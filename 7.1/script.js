f32fc8a8d5c17ef93a8fa72bbf8ad60140af3c0b
TsanAlen <tsanalen4@gmail.com>
2026-04-24 11:33:17 +0500

    Add files via upload

---
+++ 7.1/script.js
@@ -0,0 +1,7 @@
+    console.log("Helo World");
+    alert("JavaScript");
+    console.log("Helo World");
+    console.log("Helo World");
+/*Это многострочный комментарий
+весь этот текст будет проигнорирован
+компилятором*/
