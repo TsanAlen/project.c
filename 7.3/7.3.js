582e96b49a7973d25021b87ac14117697dc7556e
TsanAlen <tsanalen4@gmail.com>
2026-05-06 11:42:02 +0500

    Add files via upload

---
+++ 7.3/7.3.js
@@ -0,0 +1,30 @@
+//№1
+let a=10
+let b=5
+let c=2
+console.log(a+b+c)
+console.log((a+c)-b)
+console.log((a*b)/c)
+console.log(a%b)
+//№2
+let x=7
+console.log(x++ + ++x)
+console.log(x-- - --x)
+//№3
+let num1=4
+let num2=5
+console.log("The result is: " + (num1+num2))
+//№4
+console.log(a>b && a>0 && b>0)
+console.log(c<10 || c===2)
+console.log((a/c)!==5)
+//№5
+console.log(10=="10")
+console.log(10==="10")
+//№6
+let x1=9
+let result1=(x1%2===0 || x1%3===0) && (x1%6!==0)
+console.log(result1)
+let y=12
+let result2= !(y>10 || y<5)
+console.log(result2)
